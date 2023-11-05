2a471beede67fea79365e9b425ca79fe835c13c7
Robiul Islam <therobiulislam12@gmail.com>
2023-11-05 15:55:14 +0600

    Customizer funtion modify

--- functions.php
+++ functions.php
@@ -33,19 +33,47 @@
 
 //  Theme Function
  function ramify_customizar_register($wp_customize){
-   $wp_customize->add_section('ramify_header_area', array(
-      'title' => __('Header Area', 'ramify'),
-      'description' => __('Customize Header Area', 'ramify')
-   ));
 
+   /**
+    * This is default category Site Identity section control
+    * Add Here Logo Option for Menu Logo Change
+    * 
+    */
    $wp_customize->add_setting('ramify_logo', array(
       'default' => get_bloginfo('template_directory'). '/assets/images/logo.png',
    ));
 
    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'ramify_logo', array(
       'label' => 'Logo Upload',
-      'section' => 'ramify_header_area',
+      'description' => 'Upload Your Custom logo click on change image or default logo',
+      'section' => 'title_tagline',
       'setting' => 'ramify_logo'
    )));
+
+   /**
+    *
+    *  Create Header Area Custom Section
+    *  @since 1.0.0
+    *
+    */
+    $wp_customize->add_section('ramify_header_area', array(
+      'title' => __('Header Area', 'ramify'),
+      'description' => 'Customize Header Area'
+   ));
+   $wp_customize->add_setting('ramify_menu_position',[
+      'default' => 'right_menu'
+   ]);
+   $wp_customize->add_control('ramify_menu_position', [
+      'label'=> __('Menu Position','ramify'),
+      'description'=> 'Change you menu position what you want',
+      'section'=> 'ramify_header_area',
+      'setting'=> 'ramify_menu_position',
+      'type' => 'radio',
+      'choices' => [
+         'left_menu' => __('Left Menu', 'ramify'),
+         'center_menu' => __('Center Menu', 'ramify'),
+         'right_menu' => __('Right Menu', 'ramify')
+      ]
+   ]);
  };
  add_action('customize_register', 'ramify_customizar_register');
--- index.php
+++ index.php
@@ -1,39 +1,59 @@
-<?php 
+<?php
+
 /**
  * This template for displaying the header
  * 
  * @package ramifydigital
  */
- 
+
 ?>
 
 <!DOCTYPE html>
-<html <?php echo language_attributes();?> class="no-js"> <!-- class no-js use korar mane holo, bu default js load hobe nh -->
+<html <?php echo language_attributes(); ?> class="no-js"> <!-- class no-js use korar mane holo, bu default js load hobe nh -->
+
 <head>
     <meta charset="<?= bloginfo("charset"); ?>"> <!-- charset newyar jonnu ata use korte hobe -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?> <!-- wp_head and wp_footer kaj kore, wordpess core file load a -->
 </head>
+
 <body <?= body_class(); ?>>
 
-<header id="header" class="py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="logo">
-                    <a href="/">
-                        <img src="<?= get_theme_mod("ramify_logo"); ?>" alt="logo">
-                    </a>
+    <header id="header_area" class="py-3 navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-3 <?= get_theme_mod("ramify_menu_position"); ?>">
+                    <div class="logo">
+                        <a href="/theme-development/">
+                            <img src="<?= get_theme_mod("ramify_logo"); ?>" alt="logo">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="col-md-9"></div>
         </div>
-    </div>
-</header>
+    </header>
 
 
-<?php wp_footer(); ?> <!-- wp_head and wp_footer kaj kore, wordpess core file load a -->
+    <?php wp_footer(); ?> <!-- wp_head and wp_footer kaj kore, wordpess core file load a -->
 </body>
-</html>
 
+</html>
