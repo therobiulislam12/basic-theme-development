5e07a7781521883a9c773971e604632abef25377
Robiul Islam <therobiulislam12@gmail.com>
2023-11-09 14:25:23 +0600

    Blog Page Update and Some design changes

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <?php get_header(); ?>
 
-    <main id="main-area" class="py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <?php the_content(); ?> <!--for see page content -->
-                </div>
+<main id="main-area" class="py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <?php
+                if ( have_posts() ) {
+                    while ( have_posts() ) {
+
+                        the_post(); ?>
+
+                        <div class="blog_area bg-light-subtle p-4 rounded shadow my-2">
+                            <?php if ( has_post_thumbnail() ) : ?>
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                    <?php the_post_thumbnail(); ?>
+                                </a>
+                            <?php endif; ?>
+                            <h2>
+                                <a href="<?= get_permalink()?>"><?php the_title(); ?></a>
+                            </h2>
+                            <?php the_content(); ?>
+                        </div>
+
+                    <?php }
+                }
+                ?>
+            </div>
+            <div class="col-md-3">
+                <h2>This is sidebar</h2>
             </div>
         </div>
-    </main>
+    </div>
+</main>
 
 <?php get_footer(); ?>
