d2b59ed3dd6a77f6d4d3f699c762dd4ba734df01
Robiul Islam <therobiulislam12@gmail.com>
2023-11-13 14:59:52 +0600

    Excerpt Lenth added

--- inc/default.php
+++ inc/default.php
@@ -10,17 +10,29 @@
 
 
 // Enable Theme Title
- add_theme_support( 'title-tag' );
+add_theme_support('title-tag');
 
 
 // Add theme support for post thumbnails
 add_theme_support('post-thumbnails', ['page', 'post']);
-add_image_size('post-thumbnails', 970, 350,true);
+add_image_size('post-thumbnails', 970, 350, true);
 
 // Excerpt Length
-function ramify_excerpt_more( $more ) {
+function ramify_excerpt_more($more)
+{
     global $post;
-    $html = '<br/> <br/> <a href="'.get_permalink($post->ID) .'" class="btn btn-primary readmore-btn">' . 'Read More' . '</a>';
+    $html = ' [...] <br/> <br/> <a href="' . get_permalink($post->ID) . '" class="btn btn-primary readmore-btn">' . 'Read More' . '</a>';
     return $html;
 }
-add_filter( 'excerpt_more', 'ramify_excerpt_more');
+add_filter('excerpt_more', 'ramify_excerpt_more');
+
+
+function ramify_excerpt_length($length)
+{
+    if (is_admin()) {
+        return $length;
+    }
+    return 40;
+}
+
+add_filter('excerpt_length', 'ramify_excerpt_length', 999);
