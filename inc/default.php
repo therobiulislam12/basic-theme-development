39548aa48e804e686f2c5bbedcbe0bed00e961ab
Robiul Islam <therobiulislam12@gmail.com>
2023-11-13 14:52:26 +0600

    Added Excerpt and Readmore button on every post

--- inc/default.php
+++ inc/default.php
@@ -16,3 +16,11 @@
 // Add theme support for post thumbnails
 add_theme_support('post-thumbnails', ['page', 'post']);
 add_image_size('post-thumbnails', 970, 350,true);
+
+// Excerpt Length
+function ramify_excerpt_more( $more ) {
+    global $post;
+    $html = '<br/> <br/> <a href="'.get_permalink($post->ID) .'" class="btn btn-primary readmore-btn">' . 'Read More' . '</a>';
+    return $html;
+}
+add_filter( 'excerpt_more', 'ramify_excerpt_more');
--- index.php
+++ index.php
@@ -5,24 +5,29 @@
         <div class="row">
             <div class="col-md-9">
                 <?php
-                if ( have_posts() ) {
-                    while ( have_posts() ) {
+                if (have_posts()) {
+                    while (have_posts()) {
 
                         the_post(); ?>
 
                         <div class="blog_area bg-light-subtle p-4 rounded shadow my-4">
-                            <?php if ( has_post_thumbnail() ) : ?>
+                            <?php if (has_post_thumbnail()) : ?>
                                 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                                     <?php echo the_post_thumbnail('post-thumbnails'); ?>
                                 </a>
                             <?php endif; ?>
-                            <h2>
-                                <a href="<?= get_permalink()?>"><?php the_title(); ?></a>
-                            </h2>
-                            <?php the_content(); ?>
+                            <div class="post_details">
+                                <h2>
+                                    <a href="<?= get_permalink() ?>"><?php the_title(); ?></a>
+                                </h2>
+                                <?php the_excerpt(); ?>
+                            </div>
                         </div>
 
-                    <?php }
+                <?php
+                    }
+                } else {
+                    _e('No Post Found');
                 }
                 ?>
             </div>
