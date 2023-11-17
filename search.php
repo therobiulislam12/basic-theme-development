1faa8c5b7e9c95d0d2e9ad828c6bd6439a05ed08
Robiul Islam <therobiulislam12@gmail.com>
2023-11-17 18:17:31 +0600

    Search page added

---
+++ search.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+ * The template for displaying search result
+ * 
+ */
+
+get_header(); ?>
+
+<main id="main-area" class="py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="bg-warning-subtle p-4 rounded">
+                    <h2 class="mb-0">
+                        <?php printf(__('Search Result for: "%s"', 'ramify'), ucfirst(get_search_query())); ?>
+                    </h2>
+                </div>
+                <?php get_template_part('templates/blog_setup')?>
+
+                <div id="page_nav">
+                    <?php
+
+                    if ('ramify_pagination') {
+                        ramify_pagination();
+                    }
+                    ?>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+            <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?php get_footer(); ?>
